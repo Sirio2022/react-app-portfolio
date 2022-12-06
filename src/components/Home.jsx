1723e513b6ddb2f1cd1e84eb386f7c661367d031
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2022-12-06 18:37:51 -0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,9 +7,17 @@ const Home = () => {
 
     {/* container */}
     <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p>Hola, mi nombre es</p>
-        <h1>Juan Manuel Álvarez</h1>
-        <h2>Soy desarrollador Full Stack.</h2>
+        <p className="text-gray-300">Hola, mi nombre es</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Juan Manuel Álvarez</h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">Soy Full Stack Developer.</h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">Full Stack Developer con formación como Diseñador Web. Experiencia trabajando en NodeJS, React,
+            Redux, Mongo, Express, Posgresql, entre otras tecnologías del sector, además de metodologías ágiles. 
+            Participé en la creación de un E-commerce para el diseño y compra de remeras estampadas. 
+            Trayectoria laboral en áreas administrativas y financieras, gestionando la planeación, programación, direccionamiento de objetivos. 
+            Con pensamiento creativo, resolución de problemas, trabajo en equipo, comunicación, liderazgo y autonomía.</p>
+        <div>
+            <button className="text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-blue-600 hover:border-blue-600">Ver mi trabajo <HiArrowNarrowRight className="ml-2"/> </button>
+        </div>
 
     </div>
 
