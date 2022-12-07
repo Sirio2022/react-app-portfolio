294372fa69496f367a60668fe3b2d73adc7fde2e
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2022-12-07 15:49:03 -0500

    Brutal

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
@@ -15,7 +16,10 @@ const Home = () => {
             Me gusta programar, fan de los videojuegos y la guitarra, estoy dispuesto a aprender día a día.
             Estoy dispuesto a embarcarme en este mundo inmenso que es la programación.   </p>
         <div>
-            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-blue-600 hover:border-blue-600">Ver mi trabajo  
+            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-blue-600 hover:border-blue-600">
+            <Link  to="work"  smooth={true}  duration={500}>
+          Mira mi trabajo
+        </Link>  
             
             <span className="group-hover:rotate-90 duration-300">
             <HiArrowNarrowRight className="ml-3"/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -85,28 +85,17 @@ const Navbar = () => {
         <ul>
             <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
                 <a className="flex justify-between items-center w-full text-gray-300" 
-                href="/">
+                href="https://www.linkedin.com/in/juanmanuelalvarezb/">
                     LinkedIn <FaLinkedin size={30} />
                 </a>
             </li>
             <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
                 <a className="flex justify-between items-center w-full text-gray-300" 
-                href="/">
+                href="https://github.com/Sirio2022">
                     GitHub <FaGithub size={30} />
                 </a>
             </li>
-            <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#009966]">
-                <a className="flex justify-between items-center w-full text-gray-300" 
-                href="/">
-                    Email <HiOutlineMail size={30} />
-                </a>
-            </li>
-            <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#660033]">
-                <a className="flex justify-between items-center w-full text-gray-300" 
-                href="/">
-                    CV <BsFillPersonLinesFill size={30} />
-                </a>
-            </li>
+            
         </ul>
       </div>
     </div>
