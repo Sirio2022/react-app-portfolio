ce6a4be3d692136ef2038e7bc482417a61b1bdb1
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2022-12-07 16:23:28 -0500

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 
 const Contact = () => {
-
     
-
-
-
   return (
     <div name="contact" className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
 
-    <form method='POST' action="https://getform.io/f/a08f2c6a-10f8-45d6-b869-35932e8700c0" className='flex flex-col max-w-[600px] w-full'>
+    <form  method='POST' action="https://getform.io/f/a08f2c6a-10f8-45d6-b869-35932e8700c0" className='flex flex-col max-w-[600px] w-full'>
         <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4 text-[#8892b0]'>Contáctame</p>
             <p className='py-4 text-[#8892b0]'>Si deseas contactarme, puedes hacerlo a través de este formulario</p>
