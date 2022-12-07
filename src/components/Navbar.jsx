f8acf8414df63467a94bc88597f6862e27788bf6
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2022-12-07 15:49:51 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,5 @@
 import React, {useState} from "react";
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
-import { HiOutlineMail } from "react-icons/hi";
-import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/Logo-Transparency-(Web).png";
 import { Link } from "react-scroll";
 
