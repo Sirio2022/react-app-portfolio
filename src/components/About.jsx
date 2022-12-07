06ef70b51c877a29b41f97b6e06e6e5f216751ed
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2022-12-06 21:13:16 -0500

    Beautiful

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div name="about" className='w-full h-screen bg-[#0a192f] text-gray-300'>
-
-        <div className='flex flex-col justify-center items-center w-full h-full'>
-
-        <div className='max-w-[1000px] w-full px-4 grid grid-cols2 gap-8'>
-            <div>
-                <p>Acerca de mí</p>
-            </div>
-            <div>
-
-            </div>
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 text-[#8892b0]">
+              Acerca de mí
+            </p>
+          </div>
+          <div></div>
         </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+            <div className="sm:text-right text-4xl font-bold">
+                <p>Hola, soy JuanMa, un gusto tenerte por acá, echa un vistazo!</p>
+            </div>
+          <div>
+            <p>
+              Full Stack Developer con formación como Diseñador Web. Experiencia
+              trabajando en NodeJS, React, Redux, Mongo, Express, Posgresql,
+              entre otras tecnologías del sector, además de metodologías ágiles.
+              Participé en la creación de un E-commerce para el diseño y compra
+              de remeras estampadas. Trayectoria laboral en áreas
+              administrativas y financieras, gestionando la planeación,
+              programación, direccionamiento de objetivos. Con pensamiento
+              creativo, resolución de problemas, trabajo en equipo,
+              comunicación, liderazgo y autonomía.
+            </p>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
