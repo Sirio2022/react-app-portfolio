75b368ce7947bd27b371fedc2b926ab09a8a5ddc
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2022-12-13 09:56:40 -0500

    ok

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,37 +1,39 @@
-import React from "react";
-import { HiArrowNarrowRight } from "react-icons/hi";
-import { Link } from "react-scroll";
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
     <div name="home" className="w-full h-screen  bg-[#0a192f]">
-
-    {/* container */}
-    <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+      {/* container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-gray-300">Hola, mi nombre es</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Juan Manuel Álvarez</h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">Soy Full Stack Developer.</h2>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Juan Manuel Álvarez
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          Soy Full Stack Developer.
+        </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
-            Desarrollador FullStack graduado de Henry.
-            Me gusta programar, fan de los videojuegos y la guitarra, estoy comprometido a aprender día a día.
-            Estoy dispuesto a embarcarme en este mundo inmenso que es la programación.   </p>
+          Desarrollador FullStack graduado de Henry. Me gusta programar, fan de
+          los videojuegos y la guitarra, estoy comprometido a aprender día a
+          día. Estoy dispuesto a embarcarme en este mundo inmenso que es la
+          programación.{' '}
+        </p>
         <div>
-            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-blue-600 hover:border-blue-600">
-            <Link  to="about"  smooth={true}  duration={500}>
-          Más sobre mi
-        </Link>  
-            
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-blue-600 hover:border-blue-600">
+            <Link to="about" smooth={true} duration={500}>
+              Más sobre mi
+            </Link>
+
             <span className="group-hover:rotate-90 duration-300">
-            <HiArrowNarrowRight className="ml-3"/>
+              <HiArrowNarrowRight className="ml-3" />
             </span>
-            
-            </button>
+          </button>
         </div>
-
+      </div>
     </div>
-
-  </div>
-  )
+  );
 };
 
 export default Home;
