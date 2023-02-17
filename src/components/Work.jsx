6e9523b47cb6fbebed57fbcea8c8c05056b23cfc
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-02-17 12:07:11 -0500

    Ok

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,40 +1,58 @@
-import React from "react";
-import WorkImg from "../assets/henryShop.jpg";
-
+import React from 'react';
+import WorkImg from '../assets/henryShop.jpg';
 
 const Work = () => {
   return (
-
-
-    
     <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-
         <div className="pb-8 ">
-          <p className="text-4xl font-bold inline border-b-4 text-[#8892b0]">Proyectos</p>
-          <p className="py-6 text-[#8892b0]" >Te invito a conocer algunos de mis proyectos</p>
+          <p className="text-4xl font-bold inline border-b-4 text-[#8892b0]">
+            Proyectos
+          </p>
+          <p className="py-6 text-[#8892b0]">
+            Te invito a conocer algunos de mis proyectos
+          </p>
         </div>
-
 
         {/*Container*/}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-
-
           {/* Grid Item */}
-          <div style={{ backgroundImage: `url(${WorkImg})` }} className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div ">
-
-
+          <div
+            style={{ backgroundImage: `url(${WorkImg})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div "
+          >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-React JS Application
+                React JS Application
               </span>
               <div className="pt-8 text-center">
                 <a href="https://henry-shop.vercel.app/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
                 </a>
                 <a href="https://github.com/Sirio2022/HenryShop">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://deluxe-marzipan-52ff28.netlify.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/Sirio2022/Citas-React">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
                 </a>
               </div>
             </div>
