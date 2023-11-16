d074497f5e58418f5740e5fc0a6168b81b4b34f6
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-11-16 17:05:58 -0500

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -79,7 +79,7 @@ const Work = () => {
                 React JS Application
               </span>
               <div className="pt-8 text-center">
-                <a href="https://thunderous-kulfi-ba32be.netlify.app/">
+                <a href="https://up-task-redux-frontend.vercel.app">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
