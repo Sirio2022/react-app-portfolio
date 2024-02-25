13ca7401048287b8e52903c03791f982deb19f87
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2024-02-25 09:43:48 -0500

    Update 25/02/2024

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,6 +3,7 @@ import Ecommerce from '../assets/E-commerce.png';
 import Task from '../assets/Task.png';
 import Presupuesto from '../assets/Presupuesto.jpg';
 import Cripto from '../assets/Criptos.jpg';
+import MusicStore from '../assets/MusicStore.png';
 
 const Work = () => {
   return (
@@ -19,6 +20,36 @@ const Work = () => {
 
         {/*Container*/}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${MusicStore})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div "
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                TS-MERN-ECOMMERCE
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://ts-mern-ecommerce.onrender.com/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/Sirio2022/ts-mern-ecommerce"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${Ecommerce})` }}
