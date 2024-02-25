4fae0d15de9a8faa6140fb68e86b7f1776e99c8d
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2024-02-25 09:59:37 -0500

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -167,6 +167,31 @@ const Work = () => {
               </div>
             </div>
           </div>
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://rick-and-morty-app-pink.vercel.app/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/Sirio2022/Rick-and-Morty-APP"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
