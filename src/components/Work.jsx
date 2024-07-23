1637dfcf257e5a68a9098408aa74e804b6348e47
Juan Manuel Alvarez Bermudez <105917953+Sirio2022@users.noreply.github.com>
2024-07-23 18:33:18 -0500

    Update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,6 +5,7 @@ import Criptos from '../assets/Criptos.png';
 import MusicStore from '../assets/MusicStore.png';
 import RickMorty from '../assets/RickMorty.png';
 import ClimaApp from '../assets/Clima-app.png';
+import CalorieTracker from '../assets/Calorie-Tracker.png';
 
 const Work = () => {
   return (
@@ -42,6 +43,36 @@ const Work = () => {
                 </a>
                 <a
                   href="https://github.com/Sirio2022/clima-react-typescript"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${CalorieTracker})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div "
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                TS-REACT
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://calorie-tracker-typescript.vercel.app/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/Sirio2022/calorie-tracker-typescript"
                   target="_blank"
                 >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
