2fd3f8b35a1dc691030c46f53a6e61245fea5be5
Juan Manuel Alvarez Bermudez <105917953+Sirio2022@users.noreply.github.com>
2024-07-24 05:30:40 -0500

    Update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,6 +6,7 @@ import MusicStore from '../assets/MusicStore.png';
 import RickMorty from '../assets/RickMorty.png';
 import ClimaApp from '../assets/Clima-app.png';
 import CalorieTracker from '../assets/Calorie-Tracker.png';
+import Bebidas from '../assets/Bebidas.png';
 
 const Work = () => {
   return (
@@ -250,6 +251,36 @@ const Work = () => {
                 </a>
                 <a
                   href="https://github.com/Sirio2022/Rick-and-Morty-APP"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${Bebidas})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div "
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://bebidas-react-typescript-one.vercel.app/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/Sirio2022/bebidas-react-typescript"
                   target="_blank"
                 >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
