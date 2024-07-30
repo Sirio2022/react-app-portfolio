791799b3b56ad467b4dfa8e1e1c12075f7dabfb1
Juan Manuel Alvarez Bermudez <105917953+Sirio2022@users.noreply.github.com>
2024-07-30 07:00:53 -0500

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -32,7 +32,7 @@ const Work = () => {
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                TS-REACT
+                TS-REACT-PERN
               </span>
               <div className="pt-8 text-center">
                 <a
